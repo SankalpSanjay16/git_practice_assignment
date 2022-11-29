bd8784e71ccdaa06dcdf6293c22b05c51dea8033
Prashant <prashantsom75@gmail.com>
2022-11-29 23:26:12 +0530

    prime_and_palindrome_problem_solved

--- prime_number.js
+++ prime_number.js
@@ -4,11 +4,11 @@ let num=23;
 let count=0;
 
 for(let i=1; i<=num; i++){
-  if(i%num===0){
-       count=count+" "+i;
+  if(num%i == 0){
+     count++
 }
 
-if(count<=2){
+if(count==2){
 console.log("It is a Prime Number");
 }else{
 console.log("It is not a Prime Number");
